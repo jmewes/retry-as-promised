9f15665f8207476fdc56929d69bee9efd7cba7aa
Jan Mewes <5235584+jmewes@users.noreply.github.com>
2022-04-11 09:15:00 +0200

    Add debug logs

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-'use strict';
+"use strict";
 
-var util = require('util');
+var util = require("util");
 var format = util.format;
 
 function TimeoutError(message, err) {
   Error.call(this);
   Error.captureStackTrace(this, TimeoutError);
-  this.name = 'TimeoutError';
+  this.name = "TimeoutError";
   this.message = message;
   this.previous = err;
 }
@@ -15,7 +15,7 @@ util.inherits(TimeoutError, Error);
 
 function matches(match, err) {
   if (match === true) return true;
-  if (typeof match === 'function') {
+  if (typeof match === "function") {
     try {
       if (err instanceof match) return true;
     } catch (_) {
@@ -24,20 +24,22 @@ function matches(match, err) {
   }
   if (match === err.toString()) return true;
   if (match === err.message) return true;
-  return match instanceof RegExp
-    && (match.test(err.message) || match.test(err.toString()));
+  return (
+    match instanceof RegExp &&
+    (match.test(err.message) || match.test(err.toString()))
+  );
 }
 
 module.exports = function retryAsPromised(callback, options) {
   if (!callback || !options) {
     throw new Error(
-      'retry-as-promised must be passed a callback and a options set or a number'
+      "retry-as-promised must be passed a callback and a options set or a number"
     );
   }
 
-  if (typeof options === 'number') {
+  if (typeof options === "number") {
     options = {
-      max: options
+      max: options,
     };
   }
 
@@ -50,29 +52,37 @@ module.exports = function retryAsPromised(callback, options) {
     backoffBase: options.backoffBase === undefined ? 100 : options.backoffBase,
     backoffExponent: options.backoffExponent || 1.1,
     report: options.report || function () {},
-    name: options.name || callback.name || 'unknown'
+    name: options.name || callback.name || "unknown",
   };
 
   if (!Array.isArray(options.match)) options.match = [options.match];
-  options.report('Trying ' + options.name + ' #' + options.$current + ' at ' + new Date().toLocaleTimeString(), options);
+  options.report(
+    "Trying " +
+      options.name +
+      " #" +
+      options.$current +
+      " at " +
+      new Date().toLocaleTimeString(),
+    options
+  );
 
-  return new Promise(function(resolve, reject) {
+  return new Promise(function (resolve, reject) {
     var timeout, backoffTimeout, lastError;
 
     if (options.timeout) {
-      timeout = setTimeout(function() {
+      timeout = setTimeout(function () {
         if (backoffTimeout) clearTimeout(backoffTimeout);
-        reject(new TimeoutError(options.name + ' timed out', lastError));
+        reject(new TimeoutError(options.name + " timed out", lastError));
       }, options.timeout);
     }
 
     Promise.resolve(callback({ current: options.$current }))
       .then(resolve)
-      .then(function() {
+      .then(function () {
         if (timeout) clearTimeout(timeout);
         if (backoffTimeout) clearTimeout(backoffTimeout);
       })
-      .catch(function(err) {
+      .catch(function (err) {
         if (timeout) clearTimeout(timeout);
         if (backoffTimeout) clearTimeout(backoffTimeout);
 
@@ -80,31 +90,41 @@ module.exports = function retryAsPromised(callback, options) {
         options.report((err && err.toString()) || err, options);
 
         // Should not retry if max has been reached
+        console.log("options.$current");
+        console.log(options.$current);
+        console.log("options.max");
+        console.log(options.max);
         var shouldRetry = options.$current < options.max;
         if (!shouldRetry) return reject(err);
-        shouldRetry = options.match.length === 0 || options.match.some(function (match) {
-          return matches(match, err)
-        });
+        shouldRetry =
+          options.match.length === 0 ||
+          options.match.some(function (match) {
+            return matches(match, err);
+          });
         if (!shouldRetry) return reject(err);
 
-        var retryDelay = options.backoffBase * Math.pow(options.backoffExponent, options.$current - 1);
+        var retryDelay =
+          options.backoffBase *
+          Math.pow(options.backoffExponent, options.$current - 1);
 
         // Do some accounting
         options.$current++;
-        options.report(format('Retrying %s (%s)', options.name, options.$current), options);
+        options.report(
+          format("Retrying %s (%s)", options.name, options.$current),
+          options
+        );
 
         if (retryDelay) {
           // Use backoff function to ease retry rate
-          options.report(format('Delaying retry of %s by %s', options.name, retryDelay), options);
-          backoffTimeout = setTimeout(function() {
-            retryAsPromised(callback, options)
-              .then(resolve)
-              .catch(reject);
+          options.report(
+            format("Delaying retry of %s by %s", options.name, retryDelay),
+            options
+          );
+          backoffTimeout = setTimeout(function () {
+            retryAsPromised(callback, options).then(resolve).catch(reject);
           }, retryDelay);
         } else {
-          retryAsPromised(callback, options)
-            .then(resolve)
-            .catch(reject);
+          retryAsPromised(callback, options).then(resolve).catch(reject);
         }
       });
   });
